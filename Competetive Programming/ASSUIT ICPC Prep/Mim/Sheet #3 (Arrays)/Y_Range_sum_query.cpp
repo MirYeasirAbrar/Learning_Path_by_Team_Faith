b95db526576981cbd7e6c5e0eda3aacd42f63e50
SamiaSabaMim <samiatusesabamim@gmail.com>
2026-07-29 20:27:53 +0600

    Sheet 03. Y. Range sum query

---
+++ Competetive Programming/ASSUIT ICPC Prep/Mim/Sheet #3 (Arrays)/Y_Range_sum_query.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, Q;
+    cin >> N >> Q;
+
+    long long a[N + 1];
+    long long prefix[N + 1];
+
+    prefix[0] = 0;
+
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> a[i];
+        prefix[i] = prefix[i - 1] + a[i];
+    }
+
+    while (Q--)
+    {
+        int L, R;
+        cin >> L >> R;
+
+        cout << prefix[R] - prefix[L - 1] << endl;
+    }
+
+    return 0;
+}
