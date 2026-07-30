b099f0529e9c10bdc9c63f1294227fc4425ace49
SamiaSabaMim <samiatusesabamim@gmail.com>
2026-07-30 20:45:55 +0600

    Sheet 03. Z. Binary Search

---
+++ Competetive Programming/ASSUIT ICPC Prep/Mim/Sheet #3 (Arrays)/Z_Binary_Search.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int N,Q;
+    cin >> N >> Q;
+
+    int A[N];
+
+    for(int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+    
+    sort(A,A+N);
+
+    while(Q--)
+{
+    int X;
+    cin >> X;
+
+    if(binary_search(A, A+N, X))
+    
+    cout<< "found\n";
+    
+    else
+    cout << "not fount'\n";
+}
+    
+}
