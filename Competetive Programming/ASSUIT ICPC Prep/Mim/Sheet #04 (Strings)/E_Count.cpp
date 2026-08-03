ad0e3ca9db9816b3c6234f84c17fb634ee08f9fc
SamiaSabaMim <samiatusesabamim@gmail.com>
2026-08-03 19:50:11 +0600

    Sheet 04. E. Count

---
+++ Competetive Programming/ASSUIT ICPC Prep/Mim/Sheet #04 (Strings)/E_Count.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string S;
+
+    cin >> S;
+
+    int sum = 0;
+
+    for(int i = 0; i < S.size() ; i++)
+    {
+        sum += S[i]-'0';
+    }
+    cout<< sum <<'\n';
+}
